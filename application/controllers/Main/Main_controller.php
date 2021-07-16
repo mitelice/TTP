3247628bd9f1998cf7e6954ca2064fc6b2cbdb36
BroAnk <73092937+BroAnk@users.noreply.github.com>
2021-07-16 21:13:27 +0700

    Main and v_main
    ทำหน้าแรก และก็controller ของหน้้าแรก

---
+++ application/controllers/Main/Main_controller.php
@@ -0,0 +1,49 @@
+<?php
+/*
+* Trs_Controller
+* Form Management
+* @input  -   
+* @output -
+* @author Kunanya Singmee
+* @Create Date 2564-7-10
+*/
+?>
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require_once(dirname(__FILE__) . "/../MainController.php");
+
+class Main_controller extends MainController
+{
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	/*
+	* index
+	* 
+	* @input 
+	* @output 
+	* @author 	Kunanya Singmee
+	* @Create Date 2564-7-10
+	*/
+	function show_main()
+	{
+		$this->output('main/v_main');
+	}
+	// function index()
+
+}
+// 
---
+++ application/views/main/v_main.php
@@ -0,0 +1,201 @@
+<!--
+=========================================================
+* Argon Dashboard - v1.2.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+
+
+* Copyright  Creative Tim (http://www.creative-tim.com)
+* Coded by www.creative-tim.com
+
+
+
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
+    <!-- Favicon -->
+    <link rel="icon" href="../../assets/img/brand/favicon.png" type="image/png">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+    <!-- Icons -->
+    <link rel="stylesheet" href="../../assets/vendor/nucleo/css/nucleo.css" type="text/css">
+    <link rel="stylesheet" href="../../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+    <!-- Argon CSS -->
+    <link rel="stylesheet" href="../../assets/css/argon.css?v=1.2.0" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+
+<style>
+.button {
+  background-color:#ff0000;
+  border-radius :20px;
+  color:white;
+  padding: 5px;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+ 
+}
+
+
+</style>
+</head>
+
+<body>
+    <!-- Sidenav -->
+    <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+        <div class="scrollbar-inner">
+            <!-- Brand -->
+            <div class="sidenav-header  align-items-center">
+                <a class="navbar-brand" href="javascript:void(0)">
+                    <img src="../../assets/img/brand/blue.png" class="navbar-brand-img" alt="..." width="150"
+                        height="800">
+                </a>
+            </div>
+            <div class="navbar-inner">
+                <!-- Collapse -->
+                <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+                    <!-- Nav items -->
+                   <!-- Nav items -->
+                   <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="dashboard.html">
+                                <i class="fas fa-home text-dark"></i>
+                                <span class="nav-link-text">Home</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="icons.html">
+                                <i class="fas fa-clipboard-list text-dark"></i>
+                                <span class="nav-link-text ">Requested Form</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="map.html">
+                                <i class="fas fa-calendar-plus text-dark"></i>
+                                <span class="nav-link-text">Renewal Form</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-red active" href="profile.html">
+                                <i class="fas fa-calendar-check text-red active"></i>
+                                <span class="nav-link-text">Check Schedule</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="tables.html">
+                                <i class="fas fa-hand-point-down text-dark"></i>
+                                <span class="nav-link-text">Check Out</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.html">
+                                <i class="fas fa-user-check text-dark"></i>
+                                <span class="nav-link-text">Status Form</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="upgrade.html">
+                                <i  class="fas fa-history text-dark"></i>
+                                <span class="nav-link-text">History Form</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.html">
+                                <i class="ni ni-single-copy-04 text-dark"></i>
+                                <span class="nav-link-text">Request</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- Divider -->
+                    <hr class="my-3">
+                    <!-- Heading -->
+                 
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Main content -->
+    <div class="main-content" id="panel">
+        <!-- Topnav -->
+      
+        <!-- Header -->
+        <!-- Header -->
+        <div class="header pb-6 d-flex align-items-center"
+            style="min-height: 100px;  background-size: cover; background-position: center top;">
+            <!-- Mask -->
+            <!-- Header container -->
+
+        </div>
+        <!-- Page content -->
+        <div class="container-fluid mt--6 ">
+            <div class="row">
+
+                <div class="col-xl-12 order-xl-1">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="row align-items-center">
+                                <div class="col-8">
+                                    <h1 class="mb-0">Temporary Tag Permission System</h1>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="card-body ">
+                          <h2>Welcome รอเอาชื่อจากดาต้าเบส</h2>
+<br><br>
+                          <h6>ปัจจุบัน ทางบริษัท ได้มีการให้พนักงาน </h6>
+                          <h6>ทำการวางของในจุดต่างๆ ตามที่จัดไว้</h6>
+                          <h6>ทำให้ต้องมีการขออนุญาตวางของในพื้นที่โรงงานชั่วคราว</h6>
+                          <h6>เพื่อให้มีพื้นที่จัดเก็บของชั่วคราว</h6>
+                          <h6>ในการรอการจัดการจัดจุดวางของให้เหมาะสม</h6>
+                          <br><br>
+                          <button class="button" >เริ่มกรอกแบบฟอร์ม</button> <!--เหลือเอากดลิ้งไปหน้าอื่น-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- Argon Scripts -->
+    <!-- Core -->
+    <script src="../../assets/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="../../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/vendor/js-cookie/js.cookie.js"></script>
+    <script src="../../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+    <script src="../../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+    <!-- Argon JS -->
+    <script src="../../assets/js/argon.js?v=1.2.0"></script>
+    <script type="text/javascript">
+    $(function() {
+        $('#datetimepickerDemo').datetimepicker({
+            minDate: new Date()
+        });
+    });
+    </script>
+</body>
+
+</html>
